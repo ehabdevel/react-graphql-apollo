1d8f95787f7f1115ac3a7063e6beaba138fc91e1
Ehab Ashour <ehabdevel@gmail.com>
2018-06-24 00:03:29 +0200

    Create a React app and publish it to GitHub Pages

---
+++ src/FetchMore/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import Loading from '../Loading';
+import { ButtonUnobtrusive } from '../Button';
+
+import './style.css';
+
+const FetchMore = ({
+  loading,
+  hasNextPage,
+  variables,
+  updateQuery,
+  fetchMore,
+  children,
+}) => (
+  <div className="FetchMore">
+    {loading ? (
+      <Loading />
+    ) : (
+      hasNextPage && (
+        <ButtonUnobtrusive
+          type="button"
+          className="FetchMore-button"
+          onClick={() => fetchMore({ variables, updateQuery })}
+        >
+          More {children}
+        </ButtonUnobtrusive>
+      )
+    )}
+  </div>
+);
+
+export default FetchMore;
